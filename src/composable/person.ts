53cacaca68646b7cf18b73d2ad42b078d20a676e
Cristiano Joao <cristiano.joao@vm.co.mz>
2022-04-06 01:21:14 +0200

    chore(...): get person info by NIF

--- src/composable/person.ts
+++ src/composable/person.ts
@@ -1,3 +1,22 @@
+import {Person} from "@/api/person";
+import {PersonRequest, PersonResponse} from "@/model/person";
+
 export default function person() {
-    return {}
+    const personInstance: Person = new Person();
+
+    const getByNIF = (data: PersonRequest): Promise<void> => {
+        return new Promise((resolve, reject) => {
+
+            personInstance.getByNIF(data).then((data) => {
+                console.log(data)
+                resolve()
+            }).catch((error) => {
+                reject(error)
+            })
+
+        })
+
+    }
+
+    return {searchPersonByNIF: getByNIF}
 }
