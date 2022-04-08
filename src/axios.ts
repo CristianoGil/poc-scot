96a0e2200bd947b8ef7fdc465e2a2a56168b2d70
Cristiano Joao <cristiano.joao@vm.co.mz>
2022-04-08 10:40:57 +0200

    wip: resolve CORS error

--- src/axios.ts
+++ src/axios.ts
@@ -3,7 +3,10 @@ import {AUTH_TOKEN} from "@/utils/const";
 
 
 axios.interceptors.request.use(async (config: AxiosRequestConfig | any) => {
-    config.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`
+
+    if(config.method === "post"){  // this is  super danger, must be changed
+        config.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`
+    }
 
     return config
 })
